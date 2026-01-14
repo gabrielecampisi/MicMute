b9d0190549239564e27cd81a6a11bb8a22c38eb0
gabrielecampisi <50983392+gabrielecampisi@users.noreply.github.com>
2026-01-14 16:19:02 +0100

    Update MicMuteApp.swift

--- MicMute/MicMuteApp.swift
+++ MicMute/MicMuteApp.swift
@@ -1,3 +1,23 @@
+//
+//  MicMinuteApp.swift
+//  MicMute
+//
+//  Created by Gabriele Campisi on 2025.
+//  Copyright © 2025 Gabriele Campisi. All rights reserved.
+//
+//  CREDITS:
+//  - Lead Developer: Gabriele Campisi
+//  - UI & UX Design: Gabriele Campisi
+//  - Core Audio Integration: Gabriele Campisi
+//  - HotKey Library: Global shortcuts management.
+//
+//  LICENSE:
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+
 import Cocoa
 import SwiftUI
 import HotKey
